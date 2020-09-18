bf6edb7da1ed2f3e36140474ba0b97cc1f5a119e
Rodrigo <stuani.developer@gmail.com>
2020-09-17 22:12:05 -0300

    Iniciado novo branch e algumas alterações.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,22 @@
+/* eslint-disable prettier/prettier */
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TouchableHighlight, Alert } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
-import AsyncStorage from "@react-native-community/async-storage";
-import queryString from 'query-string'
+import AsyncStorage from '@react-native-community/async-storage';
+import queryString from 'query-string';
 import storageConfig from '../../config/storage';
 
-import { 
-  Container, Title, TitleWrapper
-} from './styles';
+import { Container, Title, TitleWrapper } from './styles';
 
-import api from '../../services/api'
+import api from '../../services/api';
 
 async function readItemStorage(key) {
-  return AsyncStorage.getItem(key, (error, value) => {
+  return AsyncStorage.getItem(key, (_error, value) => {
     return value;
   });
 }
 
 function handleInputPersist(key) {
-  
   const [value, setValue] = useState('');
 
   function readItem() {
@@ -34,48 +32,48 @@ function handleInputPersist(key) {
 
   return {
     value,
-    onChangeText: handleChangeText
+    onChangeText: handleChangeText,
   };
 }
 
 async function handleSendToClockIn() {
   const config = {
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  }
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  };
 
   const requestBody = {
-    account: "",
-    password: "",
-    identity: "",
-    origin: "",
-    key : null
-  }
+    account: '',
+    password: '',
+    identity: '',
+    origin: '',
+    key : null,
+  };
 
   await readItemStorage(storageConfig.account).then(itemValue => requestBody.account = itemValue);
   await readItemStorage(storageConfig.password).then(itemValue => requestBody.password = itemValue);
   await readItemStorage(storageConfig.identity).then(itemValue => requestBody.identity = itemValue);
   await readItemStorage(storageConfig.origin).then(itemValue => requestBody.origin = itemValue);
 
-  console.log("JSON POST: " + JSON.stringify(requestBody));
+  console.log('JSON POST: ' + JSON.stringify(requestBody));
   try {
     const response = await api.post(
-      '/batidaonline/verifyIdentification', 
+      '/batidaonline/verifyIdentification',
       queryString.stringify(requestBody),
       config
     );
-    
-    Alert.alert("Success", JSON.stringify(response.data));
+
+    Alert.alert('Success', JSON.stringify(response.data));
   } catch (error) {
     if (error.response) {
       /*
         * The request was made and the server responded with a
         * status code that falls out of the range of 2xx
         */
-      console.log("Error Data", error.response.data);
-      console.log("Error Status", error.response.status);
-      console.log("Error Headers", error.response.headers);
+      console.log('Error Data', error.response.data);
+      console.log('Error Status', error.response.status);
+      console.log('Error Headers', error.response.headers);
     } else if (error.request) {
       /*
         * The request was made but no response was received, `error.request`
@@ -87,7 +85,7 @@ async function handleSendToClockIn() {
       console.log('Error', error.message);
     }
     console.log(error);
-    Alert.alert("Error", error.message);
+    Alert.alert('Error', error.message);
   }
 }
 
@@ -105,30 +103,30 @@ export default function Main() {
 
       <TextInput
         {...account}
-        style={styles.input} 
-        placeholder="Account"
+        style={styles.input}
+        placeholder={'Account'}
       />
 
-      <TextInput 
+      <TextInput
         {...password}
-        style={styles.input} 
+        style={styles.input}
         secureTextEntry={true}
-        placeholder="Password"
+        placeholder={'Password'}
       />
 
-      <TextInput 
+      <TextInput
         {...identity}
-        style={styles.input} 
-        placeholder="Identity"
+        style={styles.input}
+        placeholder={'Identity'}
       />
 
-      <TextInput 
-        {...origin}     
-        style={styles.input} 
-        placeholder="Origin"
+      <TextInput
+        {...origin}
+        style={styles.input}
+        placeholder={'Origin'}
       />
-      
-      <TouchableHighlight 
+
+      <TouchableHighlight
         style={styles.button}
         activeOpacity={0.5}
         underlayColor="#DDC"
@@ -138,7 +136,7 @@ export default function Main() {
       </TouchableHighlight>
     </Container>
   );
-};
+}
 
 const styles = StyleSheet.create({
   input: {
@@ -146,13 +144,13 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     marginLeft: 20,
     marginBottom: 20,
-    marginRight: 20, 
-    borderWidth:0.5
+    marginRight: 20,
+    borderWidth:0.5,
   },
-  
+
   button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
+    alignItems: 'center',
+    backgroundColor: '#aaaaaa',
     padding: 10,
     marginTop: 20,
     marginLeft: 100,
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Main from './pages/Main';
 
-const Routes = createAppContainer(createSwitchNavigator({ Main }));
+const Routes = createAppContainer( createSwitchNavigator( { Main } ) );
 
 export default Routes;
